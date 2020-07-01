17023fd03dff6414c0baadaab3c4f4380ed88a0b
Miguel Aquino <jcaquino0945@gmail.com>
2020-07-01 15:09:44 +0800

    1st commit: fixed navbar and mobile cover (MOBILE)

---
+++ js/overlaymenu.js
@@ -0,0 +1,23 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+  function openNav1() {
+    document.getElementById("create").style.height = "100%";
+  }
+  function closeNav1() {
+    document.getElementById("create").style.height = "0%";
+  }
+  $("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
