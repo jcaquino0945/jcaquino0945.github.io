d8f0f8ee8e26feb9d244eb161b2c1140bc541569
Miguel Aquino <jcaquino0945@gmail.com>
2020-07-20 11:36:41 +0800

    added all mobile components (to be styled)

--- js/main.js
+++ js/main.js
@@ -11,3 +11,31 @@ function openNav() {
   function closeNav1() {
     document.getElementById("create").style.height = "0%";
   }
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
